f9cf8f77a81573dbdef9b6fa9913e053040795b3
Mihai <mcosma@gmail.com>
2023-05-20 13:04:17 -0400

    load models programmatically (#2)
    should be easier to maintain, and lets me pass custom parameters like:
    `model = llms.init("chat-bison", project=config["GOOGLE_PROJECT"], location=config["GOOGLE_ZONE"])`

--- llms/providers/base_provider.py
+++ llms/providers/base_provider.py
@@ -7,8 +7,12 @@ class BaseProvider:
     Methods will raise NotImplementedError if they are not overwritten.
     """
 
-    def __init__(self):
+    MODEL_INFO = {}
+
+    def __init__(self, model=None, api_key=None, **kwargs):
         self.latency = None
+        # to be overwritten by subclasses
+        self.api_key = api_key
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__} ({self.model})"
@@ -26,32 +30,21 @@ class BaseProvider:
 
     def compute_cost(self, prompt_tokens: int, completion_tokens: int) -> float:
         cost_per_token = self.MODEL_INFO[self.model]
-        cost = (
-            (prompt_tokens * cost_per_token["prompt"])
-            + (completion_tokens * cost_per_token["completion"])
-        ) / 1_000_000
+        cost = ((prompt_tokens * cost_per_token["prompt"]) + (completion_tokens * cost_per_token["completion"])) / 1_000_000
         cost = round(cost, 5)
         return cost
 
     def count_tokens(self):
-        raise NotImplementedError(
-            f"Count tokens is currently not supported with {self.__name__}"
-        )
+        raise NotImplementedError(f"Count tokens is currently not supported with {self.__name__}")
 
     def complete(self):
         raise NotImplementedError
 
     async def acomplete(self):
-        raise NotImplementedError(
-            f"Async complete is not yet supported with {self.__name__}"
-        )
+        raise NotImplementedError(f"Async complete is not yet supported with {self.__name__}")
 
     def complete_stream(self):
-        raise NotImplementedError(
-            f"Streaming is not yet supported with {self.__name__}"
-        )
+        raise NotImplementedError(f"Streaming is not yet supported with {self.__name__}")
 
     async def acomplete_stream(self):
-        raise NotImplementedError(
-            f"Async streaming is not yet supported with {self.__name__}"
-        )
+        raise NotImplementedError(f"Async streaming is not yet supported with {self.__name__}")
